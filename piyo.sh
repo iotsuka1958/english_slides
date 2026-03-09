07b6ba4dd8aee24fe52b36de9c3c9970036344e3
iotsuka1958 <smiley.at.circus@gmail.com>
2026-03-09 16:50:23 +0900

    minor changes

--- piyo.sh
+++ piyo.sh
@@ -1,31 +1,33 @@
 #!/usr/bin/env bash
 #以下2年pdf
-pdftk pronunciation/pronunciation_vowel.pdf cat 427 433 output 2nd_grader/upload_archive/20260303/pronunciation_20260303.pdf
-pdftk pronunciation/pronunciation_vowel.pdf cat 434 440 output 2nd_grader/upload_archive/20260304/pronunciation_20260304.pdf
-pdftk pronunciation/pronunciation_vowel.pdf cat 474 484 output 2nd_grader/upload_archive/20260305/pronunciation_20260305.pdf
-pdftk pronunciation/pronunciation_vowel.pdf cat 485 495 output 2nd_grader/upload_archive/20260306/pronunciation_20260306.pdf
-# 以下3年pdf
-pdftk pronunciation/pronunciation_vowel.pdf cat 427 433 output 3rd_grader/upload_archive/20260303/pronunciation_20260303.pdf
-pdftk pronunciation/pronunciation_vowel.pdf cat 434 440 output 3rd_grader/upload_archive/20260304/pronunciation_20260304.pdf
-pdftk pronunciation/pronunciation_vowel.pdf cat 474 484 output 3rd_grader/upload_archive/20260305/pronunciation_20260305.pdf
-pdftk pronunciation/pronunciation_vowel.pdf cat 485 495 output 3rd_grader/upload_archive/20260306/pronunciation_20260306.pdf
+pdftk pronunciation/pronunciation_vowel.pdf cat 507 517 output 2nd_grader/upload_archive/20260310/pronunciation_20260310.pdf
+pdftk pronunciation/pronunciation_vowel.pdf cat 518 528 output 2nd_grader/upload_archive/20260311/pronunciation_20260311.pdf
+pdftk pronunciation/pronunciation_vowel.pdf cat 547 557 output 2nd_grader/upload_archive/20260312/pronunciation_20260312.pdf
+pdftk pronunciation/pronunciation_vowel.pdf cat 558 568 output 2nd_grader/upload_archive/20260313/pronunciation_20260313.pdf
+pdftk pronunciation/pronunciation_vowel.pdf cat 586 596 output 2nd_grader/upload_archive/20260317/pronunciation_20260317.pdf
+pdftk pronunciation/pronunciation_vowel.pdf cat 597 607 output 2nd_grader/upload_archive/20260318/pronunciation_20260318.pdf
+pdftk pronunciation/pronunciation_vowel.pdf cat 611 612 618 output 2nd_grader/upload_archive/20260319/pronunciation_20260319.pdf
 #以下2年mp3
-cp pronunciation/audio/vowel_pool_pull_01.mp3  2nd_grader/upload_archive/20260303/pronunciation_20260303.mp3
-cp pronunciation/audio/vowel_full_fool_01.mp3  2nd_grader/upload_archive/20260304/pronunciation_20260304.mp3
-cp pronunciation/audio/vowel_e_02.mp3  2nd_grader/upload_archive/20260305/pronunciation_20260305.mp3
-cp pronunciation/audio/vowel_e_03.mp3  2nd_grader/upload_archive/20260306/pronunciation_20260306.mp3
-#以下3年mp3
-cp pronunciation/audio/vowel_pool_pull_01.mp3  3rd_grader/upload_archive/20260303/pronunciation_20260303.mp3
-cp pronunciation/audio/vowel_full_fool_01.mp3  3rd_grader/upload_archive/20260304/pronunciation_20260304.mp3
-cp pronunciation/audio/vowel_e_02.mp3  3rd_grader/upload_archive/20260305/pronunciation_20260305.mp3
-cp pronunciation/audio/vowel_e_03.mp3  3rd_grader/upload_archive/20260306/pronunciation_20260306.mp3
+cp pronunciation/audio/vowel_ei_02.mp3  2nd_grader/upload_archive/20260310/pronunciation_20260310.mp3
+cp pronunciation/audio/vowel_ei_03.mp3  2nd_grader/upload_archive/20260311/pronunciation_20260311.mp3
+cp pronunciation/audio/vowel_long_o_02.mp3  2nd_grader/upload_archive/20260312/pronunciation_20260312.mp3
+cp pronunciation/audio/vowel_long_o_03.mp3  2nd_grader/upload_archive/20260313/pronunciation_20260313.mp3
+cp pronunciation/audio/vowel_ou_02.mp3  2nd_grader/upload_archive/20260317/pronunciation_20260317.mp3
+cp pronunciation/audio/vowel_ou_03.mp3  2nd_grader/upload_archive/20260318/pronunciation_20260318.mp3
+cp pronunciation/audio/vowel_boat_bought_01.mp3  2nd_grader/upload_archive/20260319/pronunciation_20260319.mp3
 # 以下1年pdf
-pdftk pronunciation/pronunciation_consonant.pdf cat 511 517 output 1st_grader/upload_archive/20260303/pronunciation_20260303.pdf
-pdftk pronunciation/pronunciation_consonant.pdf cat 537 538 output 1st_grader/upload_archive/20260304/pronunciation_20260304.pdf
-pdftk pronunciation/pronunciation_consonant.pdf cat 539 output 1st_grader/upload_archive/20260305/pronunciation_20260305.pdf
-pdftk pronunciation/pronunciation_consonant.pdf cat 540 output 1st_grader/upload_archive/20260306/pronunciation_20260306.pdf
+pdftk pronunciation/pronunciation_consonant.pdf cat 541 output 1st_grader/upload_archive/20260310/pronunciation_20260310.pdf
+pdftk pronunciation/pronunciation_consonant.pdf cat 542 output 1st_grader/upload_archive/20260311/pronunciation_20260311.pdf
+pdftk pronunciation/pronunciation_consonant.pdf cat 543 output 1st_grader/upload_archive/20260312/pronunciation_20260312.pdf
+pdftk pronunciation/pronunciation_consonant.pdf cat 544 output 1st_grader/upload_archive/20260313/pronunciation_20260313.pdf
+pdftk pronunciation/pronunciation_consonant.pdf cat 545 538 output 1st_grader/upload_archive/20260317/pronunciation_20260317.pdf
+pdftk pronunciation/pronunciation_consonant.pdf cat 546 output 1st_grader/upload_archive/20260318/pronunciation_20260318.pdf
+pdftk pronunciation/pronunciation_consonant.pdf cat 547 output 1st_grader/upload_archive/20260319/pronunciation_20260319.pdf
 #以下1年mp3
-cp pronunciation/audio/consonant_sea_she_01.mp3  1st_grader/upload_archive/20260303/pronunciation_20260303.mp3
-cp pronunciation/audio/consonant_p_b_01.mp3  1st_grader/upload_archive/20260304/pronunciation_20260304.mp3
-cp pronunciation/audio/consonant_t_d_01.mp3  1st_grader/upload_archive/20260305/pronunciation_20260305.mp3
-cp pronunciation/audio/consonant_k_g_01.mp3  1st_grader/upload_archive/20260306/pronunciation_20260306.mp3
+cp pronunciation/audio/consonant_f_v_01.mp3  1st_grader/upload_archive/20260310/pronunciation_20260310.mp3
+cp pronunciation/audio/consonant_s_z_01.mp3  1st_grader/upload_archive/20260311/pronunciation_20260311.mp3
+cp pronunciation/audio/consonant_Th_01.mp3  1st_grader/upload_archive/20260312/pronunciation_20260312.mp3
+cp pronunciation/audio/consonant_textesh_textyogh_01.mp3  1st_grader/upload_archive/20260313/pronunciation_20260313.mp3
+cp pronunciation/audio/consonant_ts_dz_01.mp3  1st_grader/upload_archive/20260317/pronunciation_20260317.mp3
+cp pronunciation/audio/consonant_ttextesh_dtextyogh_01.mp3  1st_grader/upload_archive/20260318/pronunciation_20260318.mp3
+cp pronunciation/audio/consonant_l_r_01.mp3  1st_grader/upload_archive/20260319/pronunciation_20260319.mp3
